d3f57557d910301bd14230c4f9cd62e09bad9156
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-06-05 18:02:35 +0200

    version 0.3.2

--- wpg-user-documentation.php
+++ wpg-user-documentation.php
@@ -7,7 +7,7 @@
  * Author URI:          https://www.globalis-ms.com/
  * License:             GPL2
  *
- * Version:             0.3.0
+ * Version:             0.3.2
  * Requires at least:   4.0.0
  * Tested up to:        4.7.8
  */
